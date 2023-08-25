0823067f62858593e9ae34b8ef1e5224a968f374
Romain-Girou <romaingirou@hotmail.fr>
2023-08-25 10:55:12 +0200

    video 4

--- packages/user_repository/lib/src/firebase_user_repository.dart
+++ packages/user_repository/lib/src/firebase_user_repository.dart
@@ -1,12 +1,67 @@
+import 'dart:developer';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:user_repository/src/models/my_user.dart';
 import 'user_repo.dart';
 
 class FirebaseUserRepository implements UserRepository {
+	FirebaseUserRepository({
+		FirebaseAuth? firebaseAuth,
+	})  : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance;
 
-	// Sign In
+	final FirebaseAuth _firebaseAuth;
 
-	// Sign Out
+	@override
+  Future<MyUser> signUp(MyUser myUser, String password) async {
+    try {
+      UserCredential user = await _firebaseAuth.createUserWithEmailAndPassword(
+				email: myUser.email,
+				password: password
+			);
 
-	// Sign Up
+			myUser = myUser.copyWith(
+				id: user.user!.uid
+			);
 
-	// Reset Password
+			return myUser;
+    } catch (e) {
+      log(e.toString());
+			rethrow;
+    }
+  }
+
+	@override
+  Future<void> signIn(String email, String password) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+				email: email,
+				password: password
+			);
+    } catch (e) {
+      log(e.toString());
+			rethrow;
+    }
+  }
+
+  @override
+  Future<void> logOut() async {
+    try {
+      await _firebaseAuth.signOut();
+    } catch (e) {
+      log(e.toString());
+			rethrow;
+    }
+  }
+
+  @override
+  Future<void> resetPassword(String email) async {
+    try {
+      await _firebaseAuth.sendPasswordResetEmail(email: email);
+    } catch (e) {
+      log(e.toString());
+			rethrow;
+    }
+  }
+
+
 }
--- packages/user_repository/lib/src/user_repo.dart
+++ packages/user_repository/lib/src/user_repo.dart
@@ -1,3 +1,12 @@
+import '../user_repository.dart';
+
 abstract class UserRepository {
 	
+	Future<void> signIn(String email, String password);
+
+	Future<void> logOut();
+
+	Future<MyUser> signUp(MyUser myUser, String password);
+
+	Future<void> resetPassword(String email);
 }
